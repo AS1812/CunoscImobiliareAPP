ca5eddc13958a5bcf6b7bc1b4f35a228245adcd2
AS1812 <andrei.nicula@yahoo.com>
2022-05-28 03:56:08 +0300

    Initial Commit

--- StreamlitPg2.py
+++ StreamlitPg2.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import folium
 import json
 from streamlit_folium import folium_static
+import folium
 
 st.set_page_config(layout="wide")
 
 data_geo = json.load(open('mapz-20220528-002745.geojson'))
 
 #showing the maps
-m = folium.Map(location=[45.72,21.20])
+m = folium.Map(location=[45.72, 21.20])
 folium_static(m, width=1600, height=950)
