6547a796e89965e025e18aade807da8f96fa4aa9
AS1812 <andrei.nicula@yahoo.com>
2022-05-30 00:24:59 +0300

    Initial Commit

--- DataVisualization.py
+++ DataVisualization.py
@@ -70,12 +70,13 @@ sql = '''select id, locatieapartament as text, AVG(Pret) AS PretMediu, Min(Pret)
        group by locatieapartament;'''
 
 df4 = pd.read_sql_query(sql, sqlEngine, index_col='id')
+df4.to_csv('df41.csv')
 #df4.to_csv('timisoara_date.csv')
-
+df4_final=pd.read_csv('df41.csv')
 fname='map.geojson'
 nil = gpd.read_file(fname)
 nil = nil[['id','geometry']]
-df_final = nil.merge(df4, left_on="id", right_on="id", how="outer")
+df_final = nil.merge(df4_final, left_on="id", right_on="id", how="outer")
 #df_final.to_csv("final_csv.csv")
 
 m = folium.Map(location=[45.752, 21.22], zoom_start=13)
@@ -90,5 +91,29 @@ choropleth1 = folium.Choropleth(
     fill_opacity=0.7,
     line_opacity=0.2
 ).geojson.add_to(m)
+
+style_function = lambda x: {'fillColor': '#ffffff',
+                            'color': '#ffffff',
+                            'fillOpacity': 0.1,
+                            'weight': 0.2}
+highlight_function = lambda x: {'fillColor': '#000000',
+                                'color': '#000000',
+                                'fillOpacity': 0.50,
+                                'weight': 0.1}
+
+NIL = folium.features.GeoJson(
+    df_final,
+    style_function=style_function,
+    control=False,
+    highlight_function=highlight_function,
+    tooltip=folium.features.GeoJsonTooltip(
+        fields=['text', 'PretMediu'],
+        aliases=['Nume Regiune: ', 'Preț mediu per metru pătrat '],
+        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")
+    )
+)
+m.add_child(NIL)
+m.keep_in_front(NIL)
+folium.LayerControl().add_to(m)
 
 m.save('index.html')
--- StreamlitPg2.py
+++ StreamlitPg2.py
@@ -34,9 +34,48 @@ choropleth1 = folium.Choropleth(
 ).geojson.add_to(m)
 
 
-folium.features.GeoJson('map.geojson',
-                        name="Regiuni",
-                        popup=folium.features.GeoJsonPopup(fields=['text'], aliases=['Nume Regiune: '])
-                        ).add_to(m)
+df4_final=pd.read_csv('df41.csv')
+fname='map.geojson'
+nil = gpd.read_file(fname)
+nil = nil[['id','geometry']]
+df_final = nil.merge(df4_final, left_on="id", right_on="id", how="outer")
+#df_final.to_csv("final_csv.csv")
+
+m = folium.Map(location=[45.752, 21.22], zoom_start=13)
+
+choropleth1 = folium.Choropleth(
+    geo_data='map.geojson',
+    data=df_final,
+    columns=['id', choice_selected],
+    key_on='feature.properties.id',
+    fill_color='BuGn',
+    nan_fill_color="white",
+    fill_opacity=0.7,
+    line_opacity=0.2
+).geojson.add_to(m)
+
+style_function = lambda x: {'fillColor': '#ffffff',
+                            'color': '#ffffff',
+                            'fillOpacity': 0.1,
+                            'weight': 0.2}
+highlight_function = lambda x: {'fillColor': '#000000',
+                                'color': '#000000',
+                                'fillOpacity': 0.50,
+                                'weight': 0.1}
+
+NIL = folium.features.GeoJson(
+    df_final,
+    style_function=style_function,
+    control=False,
+    highlight_function=highlight_function,
+    tooltip=folium.features.GeoJsonTooltip(
+        fields=['text', 'PretMediu'],
+        aliases=['Nume Regiune: ', 'Preț mediu per metru pătrat '],
+        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")
+    )
+)
+m.add_child(NIL)
+m.keep_in_front(NIL)
+folium.LayerControl().add_to(m)
 
 folium_static(m, width=1600, height=900)
