ec7a09216ba6c17577045086725685df44cf237d
AS1812 <andrei.nicula@yahoo.com>
2022-06-16 09:51:10 +0300

    Initial Commit

--- DataVisualization_SelfExercises.py
+++ DataVisualization_SelfExercises.py
@@ -9,7 +9,7 @@ sqlEngine = sqlalchemy.create_engine('mysql+pymysql://AS:Bella-4-ever1556@localh
 dbConnection = sqlEngine.connect()
 tableName = "Imobiliare"
 
-"""
+
 sql = "select distinct locatieapartament, count(*) as nr_aparitii " \
       "from imobiliare " \
       "group by locatieapartament " \
@@ -52,8 +52,8 @@ scatter = px.scatter(df3,
 scatter.update_layout(xaxis_title="Preț",
                       yaxis_title="Metri pătrați", )
 scatter.show()
-"""
-"""
+
+
 sql = '''select id, locatieapartament as text, AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
        AVG(MetriPatrati) as MetriPartrati, Pret/MetriPatrati as PretMetru, count(*) as NumarAnunturi from imobiliare 
   	     where locatieapartament like 'Timisoara, zona Complex Studentesc' 
@@ -119,95 +119,23 @@ m.keep_in_front(NIL)
 folium.LayerControl().add_to(m)
 
 m.save('index.html')
-"""
 
-""" Fig update - axes
+
 fig.update_layout(
     scene=dict(
         xaxis=dict(
             backgroundcolor="rgba(0, 0, 0,0)",
-            gridcolor=color,
+            gridcolor="white",
             showbackground=True,
             zerolinecolor="white", ),
         yaxis=dict(
             backgroundcolor="rgba(0, 0, 0,0)",
-            gridcolor=color,
+            gridcolor="white",
             showbackground=True,
             zerolinecolor="white"),
         zaxis=dict(
             backgroundcolor="rgba(0, 0, 0,0)",
-            gridcolor=color,
+            gridcolor="white",
             showbackground=True,
             zerolinecolor="white", ), ),
 )
-"""
-
-sql = '''select id, count(*) as NumarAnunturi, LocatieApartament as ZonăApartament, 
-       AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
-       AVG(MetriPatrati) as MetriPartrati_InMedie, Pret/MetriPatrati as PretMediu_MetruPatrat from imobiliare 
-  	     where NumarCamere = 1 and locatieapartament in ("Timisoara, zona Complex Studentesc",
-  	     "Timisoara, zona Elisabetin", "Timisoara, zona Iosefin", 
-  	     "Timisoara, zona Blascovici", "Timisoara, zona Torontalului",
-  	     "Timisoara, zona Aradului",  "Timisoara, zona Lipovei", 
-  	     "Timisoara, zona Telegrafului", "Timisoara, zona Dorobantilor", 
-  	     "Timisoara, zona Fabric", "Timisoara, zona Cetatii") 
-	     group by ZonăApartament;'''
-
-df1 = pd.read_sql_query(sql, sqlEngine, index_col="ZonăApartament")
-df1.to_csv('sql_query_1cam.csv')
-
-sql = '''select id, count(*) as NumarAnunturi, LocatieApartament as ZonăApartament, 
-       AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
-       AVG(MetriPatrati) as MetriPartrati_InMedie, Pret/MetriPatrati as PretMediu_MetruPatrat from imobiliare 
-  	     where NumarCamere = 2 and locatieapartament in ("Timisoara, zona Complex Studentesc",
-  	     "Timisoara, zona Elisabetin", "Timisoara, zona Iosefin", 
-  	     "Timisoara, zona Blascovici", "Timisoara, zona Torontalului",
-  	     "Timisoara, zona Aradului",  "Timisoara, zona Lipovei", 
-  	     "Timisoara, zona Telegrafului", "Timisoara, zona Dorobantilor", 
-  	     "Timisoara, zona Fabric", "Timisoara, zona Cetatii") 
-	     group by ZonăApartament;'''
-
-df2 = pd.read_sql_query(sql, sqlEngine, index_col="ZonăApartament")
-df2.to_csv('sql_query_2cam.csv')
-
-sql = '''select id, count(*) as NumarAnunturi, LocatieApartament as ZonăApartament, 
-       AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
-       AVG(MetriPatrati) as MetriPartrati_InMedie, Pret/MetriPatrati as PretMediu_MetruPatrat from imobiliare 
-  	     where NumarCamere = 3 and locatieapartament in ("Timisoara, zona Complex Studentesc",
-  	     "Timisoara, zona Elisabetin", "Timisoara, zona Iosefin", 
-  	     "Timisoara, zona Blascovici", "Timisoara, zona Torontalului",
-  	     "Timisoara, zona Aradului",  "Timisoara, zona Lipovei", 
-  	     "Timisoara, zona Telegrafului", "Timisoara, zona Dorobantilor", 
-  	     "Timisoara, zona Fabric", "Timisoara, zona Cetatii") 
-	     group by ZonăApartament;'''
-
-df3 = pd.read_sql_query(sql, sqlEngine, index_col="ZonăApartament")
-df3.to_csv('sql_query_3cam.csv')
-
-sql = '''select id, count(*) as NumarAnunturi, LocatieApartament as ZonăApartament, 
-       AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
-       AVG(MetriPatrati) as MetriPartrati_InMedie, Pret/MetriPatrati as PretMediu_MetruPatrat from imobiliare 
-  	     where NumarCamere = 4 and locatieapartament in ("Timisoara, zona Complex Studentesc",
-  	     "Timisoara, zona Elisabetin", "Timisoara, zona Iosefin", 
-  	     "Timisoara, zona Blascovici", "Timisoara, zona Torontalului",
-  	     "Timisoara, zona Aradului",  "Timisoara, zona Lipovei", 
-  	     "Timisoara, zona Telegrafului", "Timisoara, zona Dorobantilor", 
-  	     "Timisoara, zona Fabric", "Timisoara, zona Cetatii") 
-	     group by ZonăApartament;'''
-
-df4 = pd.read_sql_query(sql, sqlEngine, index_col="ZonăApartament")
-df4.to_csv('sql_query_4cam.csv')
-
-sql = '''select id, count(*) as NumarAnunturi, LocatieApartament as ZonăApartament, 
-       AVG(Pret) AS PretMediu, Min(Pret) as PretMinim, MAX(Pret) as PretMaxim, 
-       AVG(MetriPatrati) as MetriPartrati_InMedie, Pret/MetriPatrati as PretMediu_MetruPatrat from imobiliare 
-  	     where NumarCamere = 5 or NumarCamere = 6 and locatieapartament in ("Timisoara, zona Complex Studentesc",
-  	     "Timisoara, zona Elisabetin", "Timisoara, zona Iosefin", 
-  	     "Timisoara, zona Blascovici", "Timisoara, zona Torontalului",
-  	     "Timisoara, zona Aradului",  "Timisoara, zona Lipovei", 
-  	     "Timisoara, zona Telegrafului", "Timisoara, zona Dorobantilor", 
-  	     "Timisoara, zona Fabric", "Timisoara, zona Cetatii") 
-	     group by ZonăApartament;'''
-
-df5 = pd.read_sql_query(sql, sqlEngine, index_col="ZonăApartament")
-df5.to_csv('sql_query_4cam.csv')
