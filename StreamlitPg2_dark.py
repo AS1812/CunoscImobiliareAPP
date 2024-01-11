6d60cccd469f6a940da708ae9340c8897d784d87
AS1812 <95412322+AS1812@users.noreply.github.com>
2024-01-11 12:08:19 +0000

    Revert code and change language

--- StreamlitPg2_dark.py
+++ StreamlitPg2_dark.py
@@ -8,13 +8,17 @@ from st_aggrid import AgGrid
 
 st.set_page_config(layout="wide")
 
+
 def set_bg():
+
+
     st.markdown(
         f"""
          <style>
          
          .stApp {{
              background: url("https://dl.dropbox.com/s/morqn6674wqgkmk/Backround_Diploma.png?dl=0"); 
+
          }}
          .css-18e3th9 {{
              padding-top: 30px;
@@ -27,6 +31,7 @@ def set_bg():
          """,
         unsafe_allow_html=True
     )
+
 
 set_bg()
 
@@ -42,17 +47,17 @@ col1, col2, col3 = st.columns((1, 1.5, 1.5))
 with col1:
     choice2 = ['1 Room', '2 Rooms', '3 Rooms', '4 Rooms']
 
-    choice_selected2 = st.radio("Select the number of rooms.", choice2,
+    choice_selected2 = st.radio("Select the number of rooms. ", choice2,
                                 help='The selected choice represents the desired number of rooms.')
 
-    choice1 = ['AveragePrice', 'MinimumPrice', 'MaximumPrice', 'AveragePrice_PerSquareMeter', 'NumberListings', 'AverageSquareMeters']
+    choice1 = ['PretMediu', 'PretMinim', 'PretMaxim', 'PretMediu_MetruPatrat', 'NumarAnunturi', 'MetriPartrati_InMedie']
 
     choice_selected1 = st.radio("Choose an option for representation. "
                                 "Prices are expressed in euros with VAT included.", choice1,
                                 help='The selected choice will be represented on the map for each region.'
                                      'For example, if "AveragePrice" is selected, the average price will be displayed in'
                                      'each region, along with its name, and a color differentiation will be made.')
-
+    
     color = st.color_picker(' ', '#3FCCE6')
 
 fname = 'map.geojson'
@@ -64,17 +69,17 @@ cam3 = pd.read_csv('sql_query_3cam.csv')
 cam4 = pd.read_csv('sql_query_4cam.csv')
 
 if choice_selected2 == '1 Room':
-    df_final = nil.merge(cam1, left_on="text", right_on="ApartmentZone", how="inner")
+    df_final = nil.merge(cam1, left_on="text", right_on="ZonăApartament", how="inner")
 if choice_selected2 == '2 Rooms':
-    df_final = nil.merge(cam2, left_on="text", right_on="ApartmentZone", how="inner")
+    df_final = nil.merge(cam2, left_on="text", right_on="ZonăApartament", how="inner")
 if choice_selected2 == '3 Rooms':
-    df_final = nil.merge(cam3, left_on="text", right_on="ApartmentZone", how="inner")
+    df_final = nil.merge(cam3, left_on="text", right_on="ZonăApartament", how="inner")
 if choice_selected2 == '4 Rooms':
-    df_final = nil.merge(cam4, left_on="text", right_on="ApartmentZone", how="inner")
+    df_final = nil.merge(cam4, left_on="text", right_on="ZonăApartament", how="inner")
 
 with col1:
     grid_response = AgGrid(
-        df_final[['ApartmentZone', choice_selected1]],
+        df_final[['ZonăApartament', choice_selected1]],
         data_return_mode='AS_INPUT',
         update_mode='MODEL_CHANGED',
         fit_columns_on_grid_load=True,
@@ -91,25 +96,28 @@ with col1:
     		  position: fixed;
     		  bottom: 50px;
     		  color: {color}
+
     		}}
 
     		</style>	
 
     		<a href="https://www.figma.com/proto/cpSU7EkLWqVp4pwA8ztZjU/Proiect_diploma?node-id=1%3A406&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A406" id="Link">
-    		FIGMA Prototype - InfoGraficeTM</a> 
+    		Prototip FIGMA - InfoGraficeTM</a> 
+
             """
     st.markdown(Link_Figma, unsafe_allow_html=True)
     choropleth1 = folium.Choropleth(
-        geo_data='map.geojson',
-        data=df_final,
-        columns=['ApartmentZone', choice_selected1],
-        key_on='feature.properties.text',
-        fill_color='Greys',
-        nan_fill_color="black",
-        line_color='White',
-        fill_opacity=0.7,
-        line_opacity=0.2
-    ).geojson.add_to(m)
+    geo_data='map.geojson',
+    data=df_final,
+    columns=['ZonăApartament', choice_selected1],
+    key_on='feature.properties.text',
+    fill_color='Greys',
+    nan_fill_color="black",
+    line_color='White',
+    fill_opacity=0.7,
+    line_opacity=0.2
+
+).geojson.add_to(m)
 
 style_function = lambda x: {'fillColor': color,
                             'color': '#ffffff',
@@ -126,8 +134,8 @@ NIL = folium.features.GeoJson(
     control=False,
     highlight_function=highlight_function,
     tooltip=folium.features.GeoJsonTooltip(
-        fields=['ApartmentZone', choice_selected1],
-        aliases=['Region Name: ', choice_selected1],
+        fields=['ZonăApartament', choice_selected1],
+        aliases=['Nume Regiune: ', choice_selected1],
         style=("background-color: white; color: #333333; font-family: arial; font-size: 13.5px; padding: 10px;")
     )
 )
@@ -142,9 +150,9 @@ if choice_selected2 == '1 Room' or choice_selected2 == '2 Rooms':
     fig = px.scatter_3d(
         df_final,
         x=choice_selected1,
-        y="ApartmentZone",
+        y="ZonăApartament",
         z=choice_selected1,
-        hover_name="ApartmentZone",
+        hover_name="ZonăApartament",
         size=choice_selected1,
         size_max=40,
         color=[color, color, color, color, color, color, color, color, color, color, color],
@@ -155,9 +163,9 @@ else:
     fig = px.scatter_3d(
         df_final,
         x=choice_selected1,
-        y="ApartmentZone",
+        y="ZonăApartament",
         z=choice_selected1,
-        hover_name="ApartmentZone",
+        hover_name="ZonăApartament",
         size=choice_selected1,
         size_max=40,
         color=[color, color, color, color, color, color, color, color, color, color],
@@ -165,9 +173,11 @@ else:
         template="plotly_dark")
 
 fig.update_layout(
+
     scene={
         'camera_eye': {"x": 2, "y": 0.6, "z": 1},
         "aspectratio": {"x": 1, "y": 1.5, "z": 0.75}
+
     },
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
@@ -175,3 +185,52 @@ fig.update_layout(
     margin=dict(l=0, r=0, t=0, b=0)
 )
 
+with col3:
+    st.plotly_chart(fig, use_container_width=True)
+
+if choice_selected2 == '1 Room' or choice_selected2 == '2 Rooms':
+    fig2 = px.bar_polar(df_final, r=choice_selected1, theta="ZonăApartament",
+                        color=[color, color, color, color, color, color, color, color, color, color, color],
+                        color_discrete_map="identity",
+                        template="plotly_dark"
+                        )
+
+else:
+    fig2 = px.bar_polar(df_final, r=choice_selected1, theta="ZonăApartament",
+                        color=[color, color, color, color, color, color, color, color, color, color],
+                        color_discrete_map="identity",
+                        template="plotly_dark"
+                        )
+
+fig2.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    autosize=True,
+    margin=dict(l=55, r=55, t=55, b=55)
+)
+
+if choice_selected2 == '1 Room' or choice_selected2 == '2 Rooms':
+
+    fig3 = px.bar(df_final, x="ZonăApartament", y=choice_selected1,
+                  color=[color, color, color, color, color, color, color, color, color, color, color],
+                  color_discrete_map="identity",
+                  template="plotly_dark")
+
+else:
+    fig3 = px.bar(df_final, x="ZonăApartament", y=choice_selected1,
+                  color=[color, color, color, color, color, color, color, color, color, color],
+                  color_discrete_map="identity",
+                  template="plotly_dark")
+
+fig3.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    autosize=True,
+    margin=dict(l=55, r=55, t=55, b=55)
+)
+
+with col2:
+    st.plotly_chart(fig3, use_container_width=True)
+
+with col2:
+    st.plotly_chart(fig2, use_container_width=True)
